37cd842b1d6cb62dd01b3a3d9317096991cf2256
Patrick <kelly.patj@gmail.com>
2023-03-01 08:30:27 -0500

    almost forgot to include the signup

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,6 @@
 // npm modules
 import { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 // components
 import LoginForm from '../../components/LoginForm/LoginForm'
@@ -20,6 +21,7 @@ const LoginPage = (props: LoginPageProps): JSX.Element => {
   return (
     <main className={styles.container}>
       <h1>Log In</h1>
+      <Link to='/signup'>Signup?</Link>
       <p>{message}</p>
       <LoginForm {...props} updateMessage={updateMessage} />
     </main>
